9ac2e9761ac45bc0f02a0ecb94d28e4c0f8d8e1e
강전우(jeonwoo_kang) <jeonwoo_kang@kolon.com>
2022-03-13 11:11:09 +0900

    gatsby with data

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,28 @@
 /** @type {import('gatsby').GatsbyConfig} */
 module.exports = {
   siteMetadata: {
-      title: `new`,
+      title: `My First Gatsby`,
       siteUrl: `https://www.yourdomain.tld`,
   },
   plugins: [
-
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: `blog`,
+        path: `${__dirname}/blog`
+      }
+    },
+    "gatsby-plugin-mdx",
+    "gatsby-transformer-sharp"
+    // {
+    //   resolve: 'gatsby-remark-prismjs',
+    //   options: {
+    //     classPrefix: "language-",
+    //     inlineCodeMarker: null,
+    //     aliases: {}
+    //   }
+    // }
   ]
 }
---
+++ src/components/layout.tsx
@@ -0,0 +1,38 @@
+import * as React from 'react'
+import { graphql, Link, useStaticQuery } from 'gatsby'
+import { container } from './layout.module.css'
+
+interface ILayoutProps {
+  pageTitle: string;
+  children: any;
+}
+
+const Layout:React.FC<ILayoutProps> = ({ pageTitle, children }) => {
+  const data = useStaticQuery(graphql`
+    query {
+      site {
+        siteMetadata {
+          title
+        }
+      }
+    }
+  `)
+  return (
+    <div className={container}>
+      <title>{pageTitle} | {data.site.siteMetadata.title}</title>
+      <header>{data.site.siteMetadata.title}</header>
+      <nav>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/about">About</Link></li>
+          <li><Link to="/blog">Blog</Link></li>
+        </ul>
+      </nav>
+      <main>
+        <h1>{pageTitle}</h1>
+        {children}
+      </main>
+    </div>
+  )
+}
+export default Layout
